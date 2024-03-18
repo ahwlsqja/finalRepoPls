4b194cf640171eaeea2bbe6d142c35d13b55b828
minibase <minibase@minibaseui-Macmini.local>
2024-03-18 17:51:47 +0900

    feat: user.entity 추가

--- src/cards/check_lists/entities/check_list.entity.ts
+++ src/cards/check_lists/entities/check_list.entity.ts
@@ -1,1 +1,25 @@
-export class CheckList {}
+import { BaseModel } from "src/common/entities/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity({
+    name: 'check_lists',
+  })
+  
+export class CheckList extends BaseModel{
+    
+    @Column({ type: 'number', unique: true, nullable: false })
+    cardId: number;
+    
+    @Column({ type: 'varchar', nullable: false })
+    title: string;
+    
+    @Column({ type: 'date', nullable: false })
+    createdAt: Date;
+    
+    @Column({ type: 'date', nullable: false })
+    updatedAt: Date;
+    
+    @Column({ type: 'number', nullable: false })
+    orderByCheck: number;
+
+}
--- src/cards/entities/card.entity.ts
+++ src/cards/entities/card.entity.ts
@@ -1,1 +1,38 @@
-export class Card {}
+import { BaseModel } from "src/common/entities/base.entity";
+import { Column, Entity } from "typeorm";
+
+
+@Entity({
+    name: 'cards'
+})
+export class Card extends BaseModel {
+
+    @Column({ type: 'number', unique: true, nullable: false })
+    columnId: number;
+
+    @Column({ type: 'varchar', nullable: false })
+    title: string;
+
+    @Column({ type: 'enum', nullable: false, default: "BLACK" })
+    color: color;
+    
+    @Column({ type: 'varchar', nullable: false })
+    content: string;
+
+    @Column({ type: 'number', nullable: false })
+    orderByCards: number;
+
+    @Column({ type: 'number', nullable: false })
+    tag: number;
+
+    @Column({ type: 'date', nullable: false })
+    endDate: Date;
+
+  }
+  enum color {
+    BLACK="BLACK",
+    RED="RED",
+    BLUE="BLUE",
+    YELLOW="YELLOW",
+    GREEN="GREEN",
+}
