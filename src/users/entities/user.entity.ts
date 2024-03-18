e335efb5708d2b384f167418aa2c79c492b1a397
유저이름 <ahwlsqja1324@gmail.com>
2024-03-18 18:08:22 +0900

    user and board merged

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+
+import { BaseModel } from "src/common/entities/base.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity({
+    name : 'users'
+})
+
+export class Users extends BaseModel {
+
+    @Column({ type : "varchar", nullable : false })
+    email : string
+
+    @Column({ type : 'varchar', nullable : false })
+    password : string
+
+    @Column({ type : 'boolean', nullable : false })
+    IsAdmin : boolean
+
+    @Column({ type : 'int', nullable : false })
+    emailtoken : number
+
+    @Column({ type : 'boolean', default : false })
+    IsVaildated : boolean
+
+    @Column({ type : 'boolean', default : false })
+    sshKey : boolean
+
+}
