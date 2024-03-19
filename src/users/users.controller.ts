43e15b26da7a21f9db3a7e9350bf2f5f079543f6
minibase <minibase@minibaseui-Macmini.local>
2024-03-19 13:58:17 +0900

    파일 삭제

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -30,6 +30,7 @@ export class UsersController {
   async update(@Param("id") id: number, @Body() body, updateUserDto: UpdateUserDto, @Req() req) {
     console.log(req.sub);
     console.log('---------');
+    console.log('---------');
     console.log(req.users);
     console.log(req.user);
     
