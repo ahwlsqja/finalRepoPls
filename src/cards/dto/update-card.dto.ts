03356245692a8ce7d46f99150575b1ff52c09211
8860k <ysmkk@naver.com>
2024-03-20 21:53:18 +0900

    cards/dto,entities,controller,service

--- src/cards/dto/update-card.dto.ts
+++ src/cards/dto/update-card.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateCardDto } from "./create-card.dto";
+import { IsNotEmpty, IsString } from "class-validator";
 
-export class UpdateCardDto extends PartialType(CreateCardDto) {}
+export class UpdateCardDto extends PartialType(CreateCardDto) {
+    @IsString()
+    @IsNotEmpty({ message: ' 제목을 입력해주세요.' })
+    title: string;
+  
+    @IsString()
+    @IsNotEmpty({ message: '내용을 입력해주세요.' })
+    content: string;
+}
