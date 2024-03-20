189ea20664fdbb3603850f3a1a02879794057693
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-03-20 13:16:58 +0900

    add: createComment, updateComment, deleteComment API 추가

--- src/cards/comments/dto/create-comment.dto.ts
+++ src/cards/comments/dto/create-comment.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
+
+export class CreateCommentDto {
+  @IsNumber()
+  cardId: number;
+
+  @IsString()
+  @IsNotEmpty({ message: "댓글 내용을 입력해주세요." })
+  @MaxLength(50, { message: "댓글은 50자 이하로 작성해주세요." })
+  content: string;
+}
